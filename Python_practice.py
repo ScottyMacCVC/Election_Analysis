436cf245a4c961484909c5f4fe589a6e4f270026
ScottyMacCVC <95444724+ScottyMacCVC@users.noreply.github.com>
2022-01-18 17:36:39 -0800

    Delete Python_practice.py

--- Python_practice.py
+++
@@ -1,8 +0,0 @@
-counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
-
-message_to_candidate = (
-    f"You received {counties_dict:,} number of votes. "
-    f"The total number of votes in the election was {total_votes:,}. "
-    f"You received {counties_dict / total_votes * 100:.2f}% of the total votes.")
-
-print(message_to_candidate)
